79435e7ce365b77a2f636bc8064e4910f9472544
SvetlanaDD <asweet.lana@gmail.com>
2022-07-06 15:59:18 +0300

    Revision task2 and task3

--- src/main/kotlin/task2/main.kt
+++ src/main/kotlin/task2/main.kt
@@ -6,6 +6,7 @@ fun main(){
 }
 
 fun people (likes: Int): String {
+    if (likes % 100 == 11) return "людям"
     if (likes % 10 == 1) return "человеку"
     else return "людям"
 }
--- src/main/kotlin/task3/main.kt
+++ src/main/kotlin/task3/main.kt
@@ -1,31 +1,21 @@
 package task3
 
 fun main (){
-    val totalPrice = 2000 * 100  // переведем руб в коп
+    val totalPrice = 11000 * 100  // переведем руб в коп
     val discountStart = 1_000 * 100
     val discountUp10 = 10_000 * 100
     val discount1 = 100 * 100
     val discount2 = 0.05
     val discount3 = 0.01
     val regularUser = true
-    val result: Int = if (totalPrice > discountStart){
-        if (totalPrice > discountUp10){
-            if (regularUser) {
-                (totalPrice * (1 - discount2) * (1 - discount3)).toInt()
-            } else {
-                (totalPrice * (1 - discount2)).toInt()
-            }
-        } else {
-            if (regularUser) {
-                ((totalPrice - discount1) * (1 - discount3)).toInt()
-            } else {
-                totalPrice - discount1
-            }
-        }
-    } else {
-        if (regularUser){
-            (totalPrice * (1 - discount3)).toInt()
-        } else totalPrice
-    }
+    var result: Int
+
+    if (totalPrice > discountStart){
+        if (totalPrice > discountUp10) result = (totalPrice * (1 - discount2)).toInt()
+        else result = totalPrice - discount1
+    } else result = totalPrice
+
+    if (regularUser) result *= (1 - discount3).toInt()
+
     println("Сумма с учетом скидки ${result / 100} руб ${result % 100} коп ")
 }
